65fd5e1adc6692c91a4e7be5e274991490b4f2b9
Michal <michal@tar.black>
2022-07-02 15:47:01 +0100

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -18,102 +18,93 @@
 
   {
     homeConfigurations.michal = home-manager.lib.homeManagerConfiguration {
-          pkgs = nixpkgs.legacyPackages.x86_64-linux;
-          modules = [ 
-          ./michal/shell.nix 
-          ./michal/dev.nix 
-          ./michal/base.nix
-          {
-            home = {
-              username = "michal";
-              homeDirectory = "/home/michal";
-            };
-          }
-        ];
-      };
-      
-      nixosConfigurations."nixos-station" = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          ./station/base.nix
-          ./station/gaming.nix
-          ./station/hardware.nix
-          ./station/music.nix
-          ./station/qmk.nix
-
-          ./common/dev.nix
-          ./common/gui-apps.nix
-          ./common/kde.nix
-          ./common/media.nix
-          ./common/nix.nix
-          ./common/personal.nix
-          ./common/yubikey.nix
-
-        ];
-      };
-
-      nixosConfigurations."nixos-laptop" = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          ./laptop/base.nix
-          ./laptop/hardware.nix
-
-          ./common/dev.nix
-          ./common/gui-apps.nix
-          ./common/kde.nix
-          ./common/media.nix
-          ./common/nix.nix
-          ./common/personal.nix
-          ./common/yubikey.nix
-
-        ];
-      };
-
-      nixosConfigurations."nixos-wsl" = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          ./wsl/base.nix
-
-          ./common/dev.nix
-          ./common/nix.nix
-          ./common/personal.nix
-          ./common/yubikey.nix
-
-          nixos-wsl.nixosModules.wsl
-          {
-            wsl.enable = true;
-            wsl.automountPath = "/mnt";
-            wsl.defaultUser = "michal";
-            wsl.startMenuLaunchers = true;
-            wsl.interop = {
-              register = true;
-              includePath = false;
-            };
-          }
-        ];
-      };
-
-      nixosConfigurations."nixos-rpi" = nixpkgs.lib.nixosSystem {
-        system = "aarch64-linux";
-        modules = [
-          ./rpi/base.nix
-          ./rpi/hardware.nix
-#         ./rpi/media.nix
-          ./rpi/personal.nix
-#         ./rpi/sway.nix
-
-#         ./server/mc-server.nix
-          ./server/gitlab.nix
-
-          ./station/qmk.nix
-
-          ./common/personal.nix
-          ./common/nix.nix
-          ./common/dev.nix
-          ./common/yubikey.nix
-
-          nixos-hardware.nixosModules.raspberry-pi-4
-        ];
-      };
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      modules = [ 
+        ./michal/shell.nix 
+        ./michal/dev.nix 
+        ./michal/base.nix
+      ];
     };
+      
+    nixosConfigurations."nixos-station" = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./station/base.nix
+        ./station/gaming.nix
+        ./station/hardware.nix
+        ./station/music.nix
+        ./station/qmk.nix
+
+        ./common/dev.nix
+        ./common/gui-apps.nix
+        ./common/kde.nix
+        ./common/media.nix
+        ./common/nix.nix
+        ./common/personal.nix
+        ./common/yubikey.nix
+      ];
+    };
+    
+    nixosConfigurations."nixos-laptop" = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./laptop/base.nix
+        ./laptop/hardware.nix
+
+        ./common/dev.nix
+        ./common/gui-apps.nix
+        ./common/kde.nix
+        ./common/media.nix
+        ./common/nix.nix
+        ./common/personal.nix
+        ./common/yubikey.nix
+      ];
+    };
+
+    nixosConfigurations."nixos-wsl" = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./wsl/base.nix
+        ./common/dev.nix
+        ./common/nix.nix
+        ./common/personal.nix
+        ./common/yubikey.nix
+
+        nixos-wsl.nixosModules.wsl
+        {
+          wsl.enable = true;
+          wsl.automountPath = "/mnt";
+          wsl.defaultUser = "michal";
+          wsl.startMenuLaunchers = true;
+          wsl.interop = {
+            register = true;
+            includePath = false;
+          };
+        }
+      ];
+    };
+
+    nixosConfigurations."nixos-rpi" = nixpkgs.lib.nixosSystem {
+      system = "aarch64-linux";
+      modules = [
+        ./rpi/base.nix
+        ./rpi/hardware.nix
+#       ./rpi/media.nix
+        ./rpi/personal.nix
+#       ./rpi/sway.nix
+
+#       ./server/mc-server.nix
+        ./server/gitlab.nix
+
+        ./station/qmk.nix
+
+        ./common/personal.nix
+        ./common/nix.nix
+        ./common/dev.nix
+        ./common/yubikey.nix
+
+        nixos-hardware.nixosModules.raspberry-pi-4
+      ];
+    };
+  };
 }
