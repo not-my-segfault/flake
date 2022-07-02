13698e38f2cd542058981870ad1194e1ae5b12e2
Michal <michal@tar.black>
2022-07-02 15:44:21 +0100

    Update base.nix

--- michal/base.nix
+++ michal/base.nix
@@ -4,8 +4,9 @@
   nixpkgs.config = { allowUnfree = true; };
 
   home = {
+    username = "michal";
+    homeDirectory = "/home/michal";
     packages = with pkgs; [ neofetch htop ncdu ];
-
     stateVersion = "22.05";
   };
 }
