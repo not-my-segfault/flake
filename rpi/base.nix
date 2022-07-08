0cc1fbcce63821adcaf058497412331ab0a0fd7e
Michal <michal@tar.black>
2022-07-08 22:17:38 +0100

    Update base.nix

--- rpi/base.nix
+++ rpi/base.nix
@@ -41,7 +41,7 @@
 
   time.timeZone = "Europe/London";
 
-  environment.systemPackages = with pkgs; [ libraspberrypi vim git helix ];
+  environment.systemPackages = with pkgs; [ libraspberrypi vim git helix htop ];
 
   system.stateVersion = "22.05";
 
