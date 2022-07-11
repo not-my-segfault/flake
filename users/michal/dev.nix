0d68f86519326b7457f3138744b26f2e45e119b9
Michal <michal@tar.black>
2022-07-11 01:29:40 +0100

    Fixed issue with ssh, for now

--- users/michal/dev.nix
+++ users/michal/dev.nix
@@ -27,32 +27,6 @@
       };
     };
     
-    ssh = {
-      compression = true;
-      matchBlocks = {
-        "Builder @ Falcon" = {
-          hostname = "repo.getcryst.al";
-          user = "builder";  
-        };
-        "Michal  @ Falcon" = {
-          hostname = "repo.getcryst.al";
-          user = "michal";
-        };
-        "Admin   @ tar" = {
-          hostname = "10.0.0.16";
-          user = "admin";
-        };
-        "Michal  @ ewok" = {
-          hostname = "10.0.0.15";
-          user = "michal";
-        };
-        "Root    @ ewok" = {
-          hostname = "10.0.0.15";
-          user = "root";
-        };
-      };
-    };
-
     fish.functions = {
       __onefetch_on_pwd_change = {
         body = "__onefetch_on_pwd_change --on-variable PWD";
@@ -80,10 +54,33 @@
   };
 
   home = {
-    file.".wsl-yubikey".source = ./configs/.wsl-yubikey;
-    file.".ssh/authorized_keys".text = ''
-      ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDAwgkEpWmDGQiOK+N5MxXdBYDIIr6DNn52UAKqY0dnxyYw1aATyChe1etqeWN/e6DH6g9yc3BvG6lV21pCVmqc3CyrxfsgQnCBU8ziUi8xk9dmdbSyZoEnETY2DGX3oWmYOlNQsbSa8IBa4iwbW5lLpdvowaka9TEmTGsNafkx8fPMhK4cdD0RsAsCg1pFCskHOxBZzrgV5HL2aMLy2Ys+4DqWOPXIY3pXsVjzdi3YVW59oXaNNd+NC8pqM3HiR2X4WS+/F5s1yIJE8G7TeC7mSqa68K6pfPAjzyHe/4ZzcyYkuOEe4KSxsPwNGGCXoYEaCMb2v7jgxWWZxedvBC1psSiFj01AAZBWl2fwSIOa88LwGtdtA/LOz5aOdHmxtZ8NwwGtx+mWz31eWUEMUEc72XMn7XdP++yvdlVsZg+bat2NryjO8+iCpz8opfqZ9r5f4EDkXFPcFMMwjUAFUUk0hv008vwhuudEwcuqgFzyt23aWOQFxTVAtD4OHYvKYCF1JkVYuZnpzDY2I2zUZ+749kYyUm+aWae+fE54FvyX0yO3M08GNizUn0X578HhICgZJlo09ewy9dIKQtsoVZPFGyr+jDqJJkJFbCkLwwxUIiijz16olrFD3UpscF+oiEVzkJ+9ClXvcbArqu1KtZ+2TrjFhk1o23ukmxZrjSEKkQ== cardno:16 179 196
-    '';
+    file = {
+      ".wsl-yubikey".source = ./configs/.wsl-yubikey;
+      ".ssh/authorized_keys".text = ''
+        ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDAwgkEpWmDGQiOK+N5MxXdBYDIIr6DNn52UAKqY0dnxyYw1aATyChe1etqeWN/e6DH6g9yc3BvG6lV21pCVmqc3CyrxfsgQnCBU8ziUi8xk9dmdbSyZoEnETY2DGX3oWmYOlNQsbSa8IBa4iwbW5lLpdvowaka9TEmTGsNafkx8fPMhK4cdD0RsAsCg1pFCskHOxBZzrgV5HL2aMLy2Ys+4DqWOPXIY3pXsVjzdi3YVW59oXaNNd+NC8pqM3HiR2X4WS+/F5s1yIJE8G7TeC7mSqa68K6pfPAjzyHe/4ZzcyYkuOEe4KSxsPwNGGCXoYEaCMb2v7jgxWWZxedvBC1psSiFj01AAZBWl2fwSIOa88LwGtdtA/LOz5aOdHmxtZ8NwwGtx+mWz31eWUEMUEc72XMn7XdP++yvdlVsZg+bat2NryjO8+iCpz8opfqZ9r5f4EDkXFPcFMMwjUAFUUk0hv008vwhuudEwcuqgFzyt23aWOQFxTVAtD4OHYvKYCF1JkVYuZnpzDY2I2zUZ+749kYyUm+aWae+fE54FvyX0yO3M08GNizUn0X578HhICgZJlo09ewy9dIKQtsoVZPFGyr+jDqJJkJFbCkLwwxUIiijz16olrFD3UpscF+oiEVzkJ+9ClXvcbArqu1KtZ+2TrjFhk1o23ukmxZrjSEKkQ== cardno:16 179 196
+      '';
+      ".ssh/config".text = ''
+        Host "Michal  @ Falcon"
+          HostName repo.getcryst.al
+          User michal
+        
+        Host "Builder @ Falcon"
+          HostName repo.getcryst.al
+          User builder
+        
+        Host "Admin   @ tar"
+          HostName 10.0.0.16
+          User admin
+        
+        Host "Michal  @ rpi"
+          HostName 10.0.0.15
+          User michal
+        
+        Host "Root    @ rpi"
+          HostName 10.0.0.15
+          User root        
+      '';
+    };
     packages = with pkgs; [
       onefetch
       w3m
